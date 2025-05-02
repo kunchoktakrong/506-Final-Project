052d368764b313c8ef9c6eb750d811d81df21b1d
kunchoktakrong <113932329+kunchoktakrong@users.noreply.github.com>
2025-05-01 23:56:56 -0400

    Update test_predict.py

--- data_functions/test_predict.py
+++ data_functions/test_predict.py
@@ -1,5 +1,9 @@
-import pandas as pd
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from data_functions.predictForest import load_data_and_train
+
 def test_model_runs():
     accuracy = load_data_and_train(test_mode=True)
-    assert accuracy > 0.5 
+    assert accuracy > 0.5
