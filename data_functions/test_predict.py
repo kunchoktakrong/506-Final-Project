4d3a5697c7446f0e1ffd4c0de9ece38cb98c5056
kunchoktakrong <113932329+kunchoktakrong@users.noreply.github.com>
2025-05-01 23:32:27 -0400

    Update test_predict.py

--- data_functions/test_predict.py
+++ data_functions/test_predict.py
@@ -1,6 +1,5 @@
 import pandas as pd
-from predictForest import load_data_and_train
-
+from data_functions.predictForest import load_data_and_train
 def test_model_runs():
     accuracy = load_data_and_train(test_mode=True)
     assert accuracy > 0.5 
